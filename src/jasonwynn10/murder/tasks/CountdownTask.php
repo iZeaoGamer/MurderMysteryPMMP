e785005c18c928e954200bcdd56357b99c5c4abb
iZeaoGamer <jameshayward14@icloud.com>
2018-10-08 18:28:01 +0100

    Update to 3.0.0-4.0.0 API (Untested)

--- src/jasonwynn10/murder/tasks/CountdownTask.php
+++ src/jasonwynn10/murder/tasks/CountdownTask.php
@@ -1,19 +1,18 @@
 <?php
 namespace jasonwynn10\murder\tasks;
 
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 
 use jasonwynn10\murder\Main;
 use jasonwynn10\murder\MurderSession;
 
-class CountdownTask extends PluginTask {
+class CountdownTask extends Task {
     /** @var MurderSession $session */
     private $session;
     public function __construct(Main $owner, MurderSession $session) {
-        parent::__construct($owner);
         $this->session = $session;
     }
-    public function onRun($currentTick) {
+    public function onRun(int $currentTick) : void{
         // TODO: Implement onRun() method.
     }
 }
